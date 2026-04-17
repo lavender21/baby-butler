b1a9d0192ec2f0a22ce4faf7dd9e15e1ced60183
ningrunting <rtning@thoughtworks.com>
2026-04-17 11:19:14 +0800

    fix: vite proxy issue

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,11 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://127.0.0.1:3001',
         changeOrigin: true
       },
       '/images': {
-        target: 'http://localhost:3001',
+        target: 'http://127.0.0.1:3001',
         changeOrigin: true
       }
     }
